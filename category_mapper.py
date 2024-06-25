631c2b0ce798ef0f75d0236af4503e90b68bd063
Bruce Lombard <blombard24@gmail.com>
2024-06-24 21:26:39 -0500

    start buiding cat_mapper function about 5 variables added

--- category_mapper.py
+++ category_mapper.py
@@ -1,9 +1,37 @@
 import pandas as pd
 import numpy as np
 
-def category_mapper(frame):
-    targ_col
-    cond = [
-        frame['Description']
+def cat_mapper(frame, targ_col, cat_col):
+    # retailer_aliases = {'amazon':['amazon','amzn mktp']}
 
+    category_retailers = {'Online Shopping':['amazon','amzn mktp', 'target.com'],
+                          'Wholesale Stores': ['costco', 'target'],
+                          'Pet Stores':['krisers'],
+                          'House Maintenance' : ['cleaning authority'],
+                          'Pet Care' : ['lucky dog daycare'],
+
+                          }
+
+    # retailer_cats = {'amazon':' Shopping'}
+
+    cond = [
+        frame[targ_col].str.contains('|'.join(category_retailers['Online Shopping']),regex=True,case=False),
+        frame[targ_col].str.contains('|'.join(category_retailers['Wholesale Stores']),regex=True,case=False),
+        frame[targ_col].str.contains('|'.join(category_retailers['Pet Stores']),regex=True,case=False),
+        frame[targ_col].str.contains('|'.join(category_retailers['House Maintenance']),regex=True,case=False),
+        frame[targ_col].str.contains('|'.join(category_retailers['Pet Care']),regex=True,case=False),
     ]
+
+    choice = [
+        'Online Shopping',
+        'Wholesale Stores',
+        'Pet Stores',
+        'House Maintenance',
+        'Pet Care',
+    ]
+
+    default_cond = frame[cat_col]
+
+    frame[cat_col] = np.select(cond, choice, default_cond)
+
+    return frame
