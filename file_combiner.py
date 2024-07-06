53ae4b20d61c5b3736349d0f583acf1146c11583
Bruce Lombard <blombard24@gmail.com>
2024-07-05 20:16:41 -0500

    added column length list for dynamic range to de-duplicate dataframe in file_combiner.py

--- file_combiner.py
+++ file_combiner.py
@@ -14,7 +14,13 @@ def transaction_compiler():
 
     df = pd.DataFrame()
 
+    col_lens = []
+
     for i in csv_file_list:
+
+        # add column counts to a list for use in de-duplication later
+        col_lens.append(pd.read_csv(i).shape[1])
+
         if 'amex' in i.lower():
             temp_df = pd.read_csv(i)
             
@@ -53,5 +59,14 @@ def transaction_compiler():
             temp_df['Date'] = pd.to_datetime(temp_df['Date'])
             temp_df = temp_df.drop('Unnamed: 0', axis=1)
             df = pd.concat([df,temp_df])
+
     df = df.reset_index(drop=True)
+
+
+    drop_col_list = df.columns[:max(col_lens)].to_list()
+
+
+    df = df.drop_duplicates(subset=drop_col_list)
+
+
     return df
